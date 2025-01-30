70de19040c9276fdc0349c63293614ea770b05d7
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-30 19:35:31 +0600

    first commit

---
+++ Day1/add_two_number.py
@@ -0,0 +1,10 @@
+num1 = int(input('Enter first number : '))
+num2 = int(input('Enter second number : '))
+
+
+def sum(num1, num2):
+    return num1+num2
+
+
+total = sum(num1, num2)
+print(total)
---
+++ Day1/squire_root.py
@@ -0,0 +1,10 @@
+# solution 1 use exponentiation
+import math
+num = 40
+# num1 = int(input('Enter a number here : '))
+sr = num**0.5
+print('The squire root of the given number is : ', sr)
+# solution 2 use math module
+num = 49
+sr = math.sqrt(num)
+print('Use math module', sr)
